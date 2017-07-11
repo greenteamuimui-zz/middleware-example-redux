c0ce1540634111113cdae708299c75e8ff67e4c9
greenteamuimui <siuding0601@gmail.com>
2017-07-10 22:24:59 -0700

    Middleware examples

---
+++ frontend/todo_redux.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+import Root from './components/root';
+//
+// const addLoggingToDispatch = (store) => {
+//   const dispatch = store.dispatch;
+//   return (next) => {
+//     return (action) => {
+//       console.log(store.getState());
+//       console.log(action);
+//       dispatch(action);
+//       console.log(store.getState());
+//       next(action);
+//     };
+//   };
+// };
+
+const addLoggingToDispatch = store => next => action => {
+  console.log(store.getState());
+  console.log(action);
+  debugger;
+  next(action);
+  console.log(store.getState());
+};
+
+// const addEmailToDispatch = store => next => action => {
+//   console.log("sending email");
+//   debugger;
+//   next(action);
+//   console.log("sent");
+// };
+
+
+const applyMiddlewares = (store, middlewares) => {
+  let dispatch = store.dispatch;
+  middlewares.forEach((middleware) => {
+    dispatch = middleware(store)(dispatch);
+  });
+  return Object.assign({}, store, { dispatch });
+};
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const preloadedState = localStorage.state ?
+    JSON.parse(localStorage.state) : {};
+  let store = configureStore(preloadedState);
+  // store = applyMiddlewares(store, [addLoggingToDispatch, addEmailToDispatch]);
+  store = applyMiddlewares(store, [addLoggingToDispatch]);
+  const root = document.getElementById('content');
+  ReactDOM.render(<Root store={store} />, root);
+});
